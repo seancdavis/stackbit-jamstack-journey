fe2a4501ff4380953e49bca3434708a98f089542
Sean C Davis <scdavis41@gmail.com>
2021-05-14 06:38:05 -0400

    Put the basics in place along with a homepage jumbotron

--- 02-static-site/tailwind.config.js
+++ 02-static-site/tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   purge: ["./www/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      colors: {
+        black: "var(--color-black)",
+        blue: "var(--color-blue)",
+        gray: {
+          DEFAULT: "var(--color-gray)",
+          light: "var(--color-gray-light)",
+        },
+        orange: "var(--color-orange)",
+        red: "var(--color-red)",
+        white: "var(--color-white)",
+        yellow: {
+          DEFAULT: "var(--color-yellow)",
+          dark: "var(--color-yellow-dark)",
+        },
+      },
+    },
+    fontFamily: {
+      default: ["Source Sans Pro", "sans-serif"],
+      headings: ["Playfair Display", "serif"],
+    },
   },
   variants: {
     extend: {},
