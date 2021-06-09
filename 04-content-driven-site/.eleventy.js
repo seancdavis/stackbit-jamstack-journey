5c82f41a42b8f94946732a7a825b7068e016efa3
Sean C Davis <scdavis41@gmail.com>
2021-06-09 10:51:19 -0400

    Copy 03 to 04

--- 04-content-driven-site/.eleventy.js
+++ 04-content-driven-site/.eleventy.js
@@ -1,13 +1,8 @@
 module.exports = function (eleventyConfig) {
   // Copy static assets over to _site directory.
-  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
-  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
-  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
-
-  return {
-    dir: {
-      input: "src",
-      layouts: "_layouts",
-    },
-  };
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("js");
+  // Return configuration object.
+  return {};
 };
